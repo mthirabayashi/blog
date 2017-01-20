e3b4864072b58b00bd2c32084418868758a29f3f
Michael Hirabayashi <mthirabayashi@gmail.com>
2017-01-20 15:21:03 -0800

    initial commit

---
+++ spec/factories/users.rb
@@ -0,0 +1,38 @@
+FactoryGirl.define do
+  factory :user do
+    trait :username do
+      username :guest
+    end
+
+    trait :email do
+      email :guest
+    end
+
+    trait :in_the_future do
+      published_at { 2.days.from_now }
+    end
+
+    trait :in_the_past do
+      published_at { 2.days.ago }
+    end
+  end
+end
+FactoryGirl.define do
+  factory :article do
+    trait :published do
+      status :published
+    end
+
+    trait :unpublished do
+      status :unpublished
+    end
+
+    trait :in_the_future do
+      published_at { 2.days.from_now }
+    end
+
+    trait :in_the_past do
+      published_at { 2.days.ago }
+    end
+  end
+end
