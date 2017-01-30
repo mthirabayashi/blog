263e2a623ce2d8c08b2526a825b5ed68cc967f00
Michael Hirabayashi <mthirabayashi@gmail.com>
2017-01-30 15:26:20 -0800

    add user controller test for #index

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,8 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  # resources :users, defaults: {format: :json}
+  constraints format: :json do
+    resources :users
+  end
   root "static_pages#root"
 end
