6638fca3571e2f9e109971b7f3dbb7b7c2e7b3d0
Michael Hirabayashi <mthirabayashi@gmail.com>
2017-01-30 12:35:54 -0800

    add basic tests for post model

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,3 +1,8 @@
-class Post
+class Post < ApplicationRecord
+  validates :body, :author_id, presence: true
 
+  belongs_to :author,
+    class_name: "User",
+    foreign_key: :author_id,
+    primary_key: :id
 end
---
+++ spec/models/post_spec.rb
@@ -0,0 +1,7 @@
+require 'rails_helper'
+
+RSpec.describe Post, type: :model do
+  let(:post) {Post.new}
+  it { should validate_presence_of(:author_id) }
+  it { should validate_presence_of(:body) }
+end
