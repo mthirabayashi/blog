a09d45f95ba5e39278362a78f7737ae601aaedca
Michael Hirabayashi <mthirabayashi@gmail.com>
2017-01-30 12:12:02 -0800

    add basic user model tests

--- Gemfile
+++ Gemfile
@@ -40,6 +40,7 @@ group :development, :test do
   gem 'byebug', platform: :mri
   gem 'rspec-rails', '~> 3.5'
   gem 'factory_girl_rails'
+  gem 'shoulda-matchers'
 end
 
 group :development do
--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -1,5 +1,16 @@
 require 'rails_helper'
 
 RSpec.describe User, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  let(:user) {User.new}
+  it { should validate_presence_of(:username) }
+  it { should validate_presence_of(:email) }
+  it { should validate_presence_of(:password_digest) }
+  it { should validate_length_of(:password). is_at_least(6). on(:create) }
+  # it { should validate_uniqueness_of(:username) }
+  # it { should validate_uniqueness_of(:email) }
+  describe 'Session Token' do
+    it 'generates session token for user' do
+      expect(user.session_token).to_not be_nil
+    end
+  end
 end
